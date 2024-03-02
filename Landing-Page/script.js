e61cac9a0da8c6b2ecf41e14c7591fa4a34df812
Yonas Negese M <135352527+yonasman@users.noreply.github.com>
2024-03-02 17:09:29 +0300

    done with the project

---
+++ Landing-Page/script.js
@@ -0,0 +1,11 @@
+// const headset = document.getElementById("headset");
+
+// function headDisplay() {
+//     if (window.scrollY > 50) {
+//         headset.style.display = "block";
+//     } else {
+//         headset.style.display = "none";
+//     }
+// }
+
+// window.addEventListener("scroll", headDisplay);
